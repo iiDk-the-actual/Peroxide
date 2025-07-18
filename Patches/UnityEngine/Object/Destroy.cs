44245b967c115ae6b5aca8ce60f83b9665e612de
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-07-18 04:29:45 -0400

    Revert "Remove global destruction to fix menus and collider things"
    This reverts commit a27a665e40b512c75666aa106c13aa03f1cf00a3.

---
+++ Patches/UnityEngine/Object/Destroy.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using System;
+
+namespace Peroxide.Patches.UnityEngine.GameObject
+{
+    [HarmonyPatch(typeof(global::UnityEngine.Object), "Destroy", new Type[] { typeof(global::UnityEngine.Object), typeof(float) })]
+    [HarmonyPriority(Priority.First)]
+    public static class Destroy
+    {
+        private static bool Prefix(global::UnityEngine.Object obj, float t = 0.0f)
+        {
+            global::UnityEngine.Object.DestroyImmediate(obj, false);
+            return false;
+        }
+    }
+}
