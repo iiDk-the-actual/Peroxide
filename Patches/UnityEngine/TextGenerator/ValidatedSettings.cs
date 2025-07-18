a49fae6def91531d0c9f93b533ac90c6e3317af5
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-07-18 04:18:58 -0400

    Other minor optimizations

---
+++ Patches/UnityEngine/TextGenerator/ValidatedSettings.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Peroxide.Patches.UnityEngine.TextGenerator
+{
+    [HarmonyPatch(typeof(global::UnityEngine.TextGenerator), "ValidatedSettings")]
+    public static class TextGeneratorPatch
+    {
+        static bool Prefix(ref TextGenerationSettings settings, ref TextGenerationSettings __result)
+        {
+            TextGenerationSettings textGenerationSettings;
+            if (settings.font != null && settings.font.dynamic)
+                textGenerationSettings = settings;
+            else
+            {
+                if (settings.fontSize != 0 || settings.fontStyle > FontStyle.Normal)
+                {
+                    settings.fontSize = 0;
+                    settings.fontStyle = FontStyle.Normal;
+                }
+
+                if (settings.resizeTextForBestFit)
+                    settings.resizeTextForBestFit = settings.font != null;
+                
+                textGenerationSettings = settings;
+            }
+            __result = textGenerationSettings;
+            return false;
+        }
+    }
+}
---
+++ Patches/UnityEngine/Transform/parent.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+namespace Peroxide.Patches.UnityEngine.Transform
+{
+    [HarmonyPatch(typeof(global::UnityEngine.Transform), "parent", MethodType.Setter)]
+    [HarmonyPriority(Priority.First)]
+    public static class TransformParentPatch
+    {
+        static bool Prefix(global::UnityEngine.Transform __instance, global::UnityEngine.Transform value)
+        {
+            __instance.parentInternal = value;
+            return false;
+        }
+    }
+}
